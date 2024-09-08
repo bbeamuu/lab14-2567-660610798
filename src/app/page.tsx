4f245516ae5a128da9cf34fd3dc963024d3efb12
Beam <b3bb0z@gmail.com>
2024-09-08 20:39:12 +0700

    so late

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,65 @@
+
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Container, Title, Rating, Textarea, Button, Text, Divider, Group, Pagination, Space } from "@mantine/core";
+
+import Footer from "@components/Footer";
 
 export default function Home() {
+  
+
   return (
     <Container size="600px">
-      <Title order={2}>Food Review 🍕</Title>
+      
+      <Title order={2} >
+        Food Review 🍕
+      </Title>
+      <Space h="sm"/>
 
-      <Text ta="center" my="sm">
-        Copyright © 2024 Chayanin Suatap 650610560
+      <Title order={4}>Your rating</Title>
+      <Rating defaultValue={0} size="lg" />
+      <Space h="sm"/>
+
+      <Textarea
+        label= "Your review"
+        placeholder="Do you enjoy eating?"
+        autosize
+        minRows={3}
+      />
+      <Space h="sm"/>
+
+      <Button color="orange">
+        Submit Review
+      </Button>
+      <Space h="sm"/>
+      <Divider/>
+      
+      <Space h="sm"/>
+      <Group gap="lg" justify="center">
+      <Title order={4}>Elon Musk</Title>
+      <Rating value={5} readOnly/>
+      </Group>
+      <Text fz={15} ta = "center" c="rgb(134 , 142 , 150)" >Best pizza in this world. I give you X score.</Text>
+
+      <Space h="sm"/>
+      <Divider/>
+
+
+      <Space h="sm"/>
+      <Group gap="lg" justify="center">
+      <Title order={4}>Mark Zuck</Title>
+      <Rating value={4} readOnly/>
+      </Group>
+      <Text fz={15} ta = "center" c="rgb(134 , 142 , 150)" >My favourite part is pepperoni</Text>
+
+      <Group justify="center" mt="lg">
+        <Pagination total={20} color="orange" />
+      </Group>
+
+      <Space h="sm"/>
+      <Text fz={15} ta = "center" c="rgb(134 , 142 , 150)" >
+        <Footer year = {2024} name = "Supakorn Booranachart" id = {660610798} />
       </Text>
+      
     </Container>
   );
 }
